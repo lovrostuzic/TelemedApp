b3443f5258452636379bbb29676bcc4080516d84
lovrostuzic <goldstudiostu@gmail.com>
2023-11-27 00:10:49 +0100

    up

--- static/functions.js
+++ static/functions.js
@@ -68,4 +68,27 @@ function popuniRedove() {
 
 window.onload = function() {
   popuniRedove();
-};
+}
+function patientNewHistory() {
+ window.location.href = "patientHistory.html";
+
+  var sistolickiTlak = document.getElementById('sistlak').value;
+  var dijastolickiTlak = document.getElementById('dijtlak').value;
+  var brojOtkucajaSrca = document.getElementById('heartrate').value;
+
+  var table = document.getElementById('patient_table');
+  var newRow = table.insertRow(-1);
+
+  var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+  var cell3 = newRow.insertCell(2);
+  var cell4 = newRow.insertCell(3);
+  var cell5 = newRow.insertCell(4);
+
+  cell1.textContent = 'Novi red';
+  cell2.textContent = sistolickiTlak;
+  cell3.textContent = dijastolickiTlak;
+  cell4.textContent = brojOtkucajaSrca;
+  cell5.textContent = 'Ostali podaci...';
+
+  }
