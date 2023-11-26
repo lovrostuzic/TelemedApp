da0da022f65eabdfe8843e448ce02088a23ab03d
lovrostuzic <goldstudiostu@gmail.com>
2023-11-26 23:59:39 +0100

    up

--- static/functions.js
+++ static/functions.js
@@ -19,3 +19,53 @@ window.location.href = "patientHistory.html";
 function pregledZapisa(){
 window.location.href = "doctorPatientHistory.html";
 }
+function popuniRedove() {
+  var table = document.getElementById('patient_table');
+
+     var newRow = table.insertRow(-1);
+
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    var cell4 = newRow.insertCell(3);
+    var cell5 = newRow.insertCell(4);
+
+    cell1.textContent = "a";
+    cell2.textContent ="a" ;
+    cell3.textContent = "a";
+    cell4.textContent = "a";
+    cell5.textContent = "a";
+
+      var newRow = table.insertRow(-1);
+
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+        var cell4 = newRow.insertCell(3);
+        var cell5 = newRow.insertCell(4);
+
+        cell1.textContent = "a";
+        cell2.textContent ="a" ;
+        cell3.textContent = "a";
+        cell4.textContent = "a";
+        cell5.textContent = "a";
+  var newRow = table.insertRow(-1);
+
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    var cell4 = newRow.insertCell(3);
+    var cell5 = newRow.insertCell(4);
+
+    cell1.textContent = "a";
+    cell2.textContent ="a" ;
+    cell3.textContent = "a";
+    cell4.textContent = "a";
+    cell5.textContent = "a";
+
+
+}
+
+window.onload = function() {
+  popuniRedove();
+};
